08681a7b7b5c64f18858702d28964bffc7512e48
Shane Reeve <Shanereeve@gmail.com>
2018-04-03 15:19:26 -0600

    Editted tests

--- test/dummy.test.js
+++ test/dummy.test.js
@@ -3,13 +3,13 @@ import Server from '../src'
 describe(`initial`, () => {
 
 
-  let server
-  beforeEach(async () => {
-    server = await Server()
-  })
+let server
+beforeEach(async () => {
+  server = await Server()
+})
 
-  it(`has valid server`, async () => {
-    const { statusCode } = await server.inject('/')
-    expect(statusCode).to.equal(200)
-  })
+it(`has valid server`, async () => {
+  const { statusCode } = await server.inject('/')
+  expect(statusCode).to.equal(200)
+})
 })
